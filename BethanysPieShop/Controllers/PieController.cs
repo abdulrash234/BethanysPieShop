f889402ac878b05d004073c1e9702c8b4aa2639a
Abdulrashid Mohammed <abmoh@dfds.com>
2020-07-23 12:49:41 +0200

    Add project files.

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,30 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel()
+                {CurrentCategory = "cheese cakes", 
+                    Pies = _pieRepository.Pies
+
+                };
+            return View(piesListViewModel);
+        }
+    }
+}
